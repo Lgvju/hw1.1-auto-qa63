9bdc5a1d1537048b3241548e07ec6ed5585164a3
OEM <gurleva_ludmila@mail.ru>
2023-09-13 18:54:15 +0300

    TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,47 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldAdvice100() {
+
+        CashbackHackService cashback = new CashbackHackService();
+
+        int amount = 1900;
+
+        int expected = 100;
+        int actual = cashback.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAdvice1000() {
+
+        CashbackHackService cashback = new CashbackHackService();
+
+        int amount = 0;
+
+        int expected = 1000;
+        int actual = cashback.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAdvice0() {
+
+        CashbackHackService cashback = new CashbackHackService();
+
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = cashback.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+}
